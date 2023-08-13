a2cf63bcd0fe5f3a1e455b8b41f1986278c47f6e
nschied <Nicolas.SCHIED@ajc-formation.fr>
2023-08-13 16:53:50 +0200

    Only test python version

--- test/testLucipy.py
+++ test/testLucipy.py
@@ -15,39 +15,39 @@ class TestLucipy(unittest.TestCase):
         expMsg = '41545441434B204154204441574E2020'.encode('UTF-8')
         self.assertEqual(expMsg,msg)
         
-    def test_hex2a_02(self):
+    def test_hex2a_01(self):
         key = '53555045525345435245542020202020'
         rawkey = lucipy.hex2a(key)
         expKey = 'SUPERSECRET'.encode('UTF-8')
         self.assertEqual(expKey,rawkey)
         
-    def test_hex2a_03(self):
+    def test_hex2a_02(self):
         msg = '41545441434B204154204441574E2020'
         rawmsg = lucipy.hex2a(msg)
         expMsg = 'ATTACK AT DAWN'.encode('UTF-8')
         self.assertEqual(expMsg,rawmsg)
 
-    def test_cipher_10(self):
+    def test_cipher_01(self):
         key = '0123456789ABCDEFFEDCBA9876543210'
         msg = 'AAAAAAAAAAAAAAAABBBBBBBBBBBBBBBB'
         expected = '7C790EFDE03679E4BF28FE2D199E41A0'.encode('UTF-8')
         resulted = lucipy.cipher(0,key,msg)
         self.assertEqual(resulted,expected)
         
-    def test_cipher_11(self):
+    def test_cipher_02(self):
         key = '0123456789ABCDEFFEDCBA9876543210'
         msg = '7C790EFDE03679E4BF28FE2D199E41A0'
         expected = 'AAAAAAAAAAAAAAAABBBBBBBBBBBBBBBB'.encode('UTF-8')
         resulted = lucipy.cipher(1,key,msg)
         self.assertEqual(resulted,expected)
         
-    def test_cipher_12(self):
+    def test_cipher_03(self):
         key = '0123456789ABCDEFFEDCBA9876543210'
         msg = '7C790EFDE03679E4BF28FE2D199E41A0'
         resulted = lucipy.cipher(1,key,lucipy.cipher(0,key,msg))
         self.assertEqual(resulted,msg.encode('UTF-8'))
         
-    def test_cipher_13(self):
+    def test_cipher_04(self):
         key = '0123456789ABCDEFFEDCBA9876543210'
         msg = '7C790EFDE03679E4BF28FE2D199E41A0'
         resulted = lucipy.cipher(0,key,lucipy.cipher(1,key,msg))
