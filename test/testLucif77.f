3ebd984de3b95ebbfc870b43e58c7147b69a84bb
nschied <Nicolas.SCHIED@ajc-formation.fr>
2023-08-13 09:30:39 +0200

    Add validation tests

--- test/testLucif77.f
+++ test/testLucif77.f
@@ -16,72 +16,71 @@ C     ******************************************************************
 C     ******************************************************************
       SUBROUTINE TEST1()
       EXTERNAL CIPHER
-      CHARACTER*32 KEY, MSG
+      CHARACTER*32 KEY, MSG, REF
 
 	  LOGICAL ISOK
 	  ISOK = .FALSE.
 
       KEY = '0123456789ABCDEFFEDCBA9876543210'
       MSG = 'AAAAAAAAAAAAAAAABBBBBBBBBBBBBBBB'
-
-C      PRINT *, 'KEY: ', KEY
-C      PRINT *, 'MSG: ', MSG
+	  REF = '7C790EFDE03679E4BF28FE2D199E41A0'
 
       CALL CIPHER(0, KEY, MSG)
-C      PRINT *, 'ENCRYPTED: ', MSG
-C      PRINT *, 'EXPECTED:  ', '7C790EFDE03679E4BF28FE2D199E41A0'
-      IF(MSG.EQ.'7C790EFDE03679E4BF28FE2D199E41A0') THEN
+
+      IF(MSG.EQ.REF) THEN
 			  ISOK = .TRUE.
 	  ELSE
 			  ISOK = .FALSE.
       ENDIF
 
       CALL CIPHER(1, KEY, MSG)
-C      PRINT *, 'DECRYPTED: ', MSG
 
-      IF(MSG.EQ.'AAAAAAAAAAAAAAAABBBBBBBBBBBBBBBB') THEN
+      IF(MSG.EQ.MSG) THEN
               ISOK = .TRUE.
 	  ELSE
 			  ISOK = .FALSE.
       ENDIF
 	  
 	  IF(ISOK) THEN
-              PRINT*, '>>> TEST 1 : OK'
+              PRINT*, '>>>> F77 TEST 1: OK'
       ELSE
-              PRINT*, '>>> TEST 1 : KO'
+              PRINT*, '>>>> F77 TEST 1: KO'
       ENDIF
 
       END
 C     ******************************************************************
       SUBROUTINE TEST2()
       EXTERNAL CIPHER
-      CHARACTER*32 KEY, MSG
+      CHARACTER*32 KEY, MSG, REF
       INTEGER I
-
-      PRINT *, '>>> TEST 2'
 
       WRITE (KEY, 100) (INT(RAND() * 16), I = 1, 32)
 
       MSG = 'AAAAAAAAAAAAAAAABBBBBBBBBBBBBBBB'
+	  REF = MSG
 
-      PRINT *, 'KEY: ', KEY
-      PRINT *, 'MSG: ', MSG
       CALL CIPHER(0, KEY, MSG)
-      PRINT *, 'ENCRYPTED: ', MSG
-
       CALL CIPHER(1, KEY, MSG)
-      PRINT *, 'DECRYPTED: ', MSG
+
+	  IF(MSG.EQ.REF) THEN
+		PRINT*, '>>>> F77 TEST 2: OK'
+	  ELSE
+		PRINT*, '>>>> F77 TEST 2: KO'  
+	  ENDIF
 
   100 FORMAT (32Z1.1)
       END
 C     ******************************************************************
       SUBROUTINE TEST3()
       EXTERNAL CIPHER
-      CHARACTER*32 KEY, MSG
-      INTEGER I, J
-      REAL T1, T2
+      CHARACTER*32 KEY, MSG, REF
+      INTEGER I, J , K , LL
+      REAL T1, T2, DT
+	  T1=0.
+	  T2=0.
 
       MSG = 'AAAAAAAAAAAAAAAABBBBBBBBBBBBBBBB'
+	  REF = MSG
 
       CALL CPU_TIME(T1)
 
@@ -90,13 +89,26 @@ C     ******************************************************************
       CALL CIPHER(0, KEY, MSG)
       CALL CIPHER(1, KEY, MSG)
    10 CONTINUE
-
+   
+C     Loop to consume CPU time
+C     LL = 25000
+      LL = 10
+      K=0
+      DO 666 I=1,LL
+	    K=K+I
+	    DO 667 J=1,LL
+		   K=K-J
+  667   CONTINUE
+  666 CONTINUE
+  
       CALL CPU_TIME(T2)
-      IF((T2-T1).LE.1.) THEN
-              PRINT*, '>>> TEST 3 : OK'
+	  DT = ABS(T2-T1)
+
+      IF((DT.LE.1.).AND.(MSG.EQ.REF)) THEN
+              PRINT*, '>>>> F77 TEST 3: OK'
       ELSE
-              PRINT*, '>>> TEST 3 : KO'
-       ENDIF
+              PRINT*, '>>>> F77 TEST 3: KO'
+      ENDIF
 
   100 FORMAT (32Z1.1)
   200 FORMAT (' 1000 RUNS IN ',F12.8,' SECONDS')
@@ -105,27 +117,23 @@ C     ******************************************************************
       SUBROUTINE TEST4()
       EXTERNAL A2HEX, CIPHER, HEX2A
       CHARACTER*16 RAWKEY, RAWMSG
-      CHARACTER*32 KEY, MSG
-
-      PRINT *, '>>> TEST 4'
+      CHARACTER*32 KEY, MSG, REF, RESU
+	  LOGICAL ISOK
 
       RAWKEY = 'SUPERSECRET'
       RAWMSG = 'ATTACK AT DAWN'
+	  REF = RAWMSG
 
       CALL A2HEX(RAWKEY, KEY)
       CALL A2HEX(RAWMSG, MSG)
-
-      PRINT *, 'KEY: ', KEY
-      PRINT *, 'MSG: ', MSG
-
       CALL CIPHER(0, KEY, MSG)
-
-      PRINT *, 'ENCRYPTED: ', MSG
-      PRINT *, 'ASCII:     ', RAWMSG
-
       CALL CIPHER(1, KEY, MSG)
-      CALL HEX2A(MSG, RAWMSG)
+      CALL HEX2A(MSG, RESU)
+	  
+	  IF(RESU.EQ.REF) THEN
+		PRINT*,  '>>>> F77 TEST 4: OK'
+	  ELSE
+	    PRINT*,  '>>>> F77 TEST 4: OK'
+	  ENDIF
 
-      PRINT *, 'DECRYPTED: ', MSG
-      PRINT *, 'ASCII:     ', RAWMSG
       END
