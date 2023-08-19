c342a83c28577081f39a7f839e2508c25b2c9937
nschied <Nicolas.SCHIED@ajc-formation.fr>
2023-08-19 14:34:46 +0200

    Test GitHub actions

--- test/testLucipy.py
+++ test/testLucipy.py
@@ -56,7 +56,8 @@ class TestLucipy(unittest.TestCase):
     def test_encode_decode(self):
         rawkey = '25Novembre1981'
         key = lucipy.a2hex(rawkey)
-        rawmsg = 'Naissance de moi'
+        #rawmsg = 'Naissance de moi'
+        rawmsg = 'Naissance de Dieu'
         msg = lucipy.a2hex(rawmsg)
         self.assertEqual(  lucipy.hex2a( lucipy.cipher(0,key,lucipy.cipher(1,key,msg))),rawmsg.encode('UTF-8'))
         
